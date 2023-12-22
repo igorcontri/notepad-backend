dee4f3959dfaf25e23e9ad8bc82d4e37f11691f4
Igor Contri <igorcontri@gmail.com>
2023-12-22 18:06:42 -0300

    Update user settings and fixing "updated_at"

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqLiteConnection = require("../database/sqlite");
@@ -25,12 +26,68 @@ class UsersController {
             throw new AppError("This email already exists.");
         }
 
+        const hashedPassword = await hash(password, 8);
+
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         );
 
         return res.status(201).json();
+    }
+
+    async update(req, res) {
+        const { name, email, password, old_password } = req.body;
+        const { id } = req.params;
+
+        const database = await sqLiteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+            id,
+        ]);
+
+        if (!user) {
+            throw new AppError("User not found.");
+        }
+
+        const userWithUpdateEmail = await database.get(
+            "SELECT * FROM users WHERE email = (?)",
+            [email]
+        );
+
+        if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new AppError("This email already exists.");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        if (password && !old_password) {
+            throw new AppError("Error! Old password required");
+        }
+
+        if (password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if (!checkOldPassword) {
+                throw new AppError("Error! Wrong Password");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
+        await database.run(
+            `
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            password = ?,
+            updated_at = DATETIME('now')
+            WHERE id = ?
+        `,
+            [user.name, user.email, user.password, id]
+        );
+
+        return res.status(200).json();
     }
 }
 
