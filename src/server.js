efb40a62652b571850c3c8bb41bb4639d75c7f1d
Igor Contri <igorcontri@gmail.com>
2024-01-24 17:08:39 -0300

    test

--- src/server.js
+++ src/server.js
@@ -12,7 +12,19 @@ const routes = require("./routes");
 migrationsRun();
 
 const app = express();
+
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://seu-domínio-netlify.netlify.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
