3ab37f2de564518368f8b7cf94c5c5cf8df2ea20
Igor Contri <igorcontri@gmail.com>
2024-01-29 09:24:26 -0300

    UserRepository

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,20 @@
+class UserRepository {
+  async findByEmail(email) {
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE email = (?)", [
+      email,
+    ]);
+  }
+
+  async create(name, email, password) {
+    const database = await sqliteConnection();
+    const userId = await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+
+    return { id: userId };
+  }
+}
+
+module.exports = UserRepository;
